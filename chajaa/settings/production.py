fd1cdc90e03da64f93b7c4dad50b45ac60f149dc
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-03-28 16:41:27 +0200

    Use correct module for DEFAULT_FILE_STORAGE

--- chajaa/settings/production.py
+++ chajaa/settings/production.py
@@ -51,7 +51,7 @@ if DEFAULT_STORAGE_DSN:
     DefaultStorageClass = dsn_configured_storage_class('DEFAULT_STORAGE_DSN')
 
     # Django's DEFAULT_FILE_STORAGE requires the class name
-    DEFAULT_FILE_STORAGE = 'myapp.settings.DefaultStorageClass'
+    DEFAULT_FILE_STORAGE = 'chajaa.settings.production.DefaultStorageClass'
 else:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
