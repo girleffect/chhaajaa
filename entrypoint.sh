6b5279c2884511c8dd45fff4817f8fcbf958dcee
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-04-06 13:22:52 +0200

    Make entrypoint.py replace Domain placeholder in nginx conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput 
+
+if [ -n "$DOMAIN" ]; then
+    sed -i "s/{{DOMAIN}}/${DOMAIN}/" /etc/nginx/conf.d/django.conf
+else
+    sed -i "s/{{DOMAIN}}/_/" /etc/nginx/conf.d/django.conf
+fi
+
 nginx -g 'daemon off;' &
 gosu wagtail gunicorn chajaa.wsgi:application --config /etc/gunicorn/config.py --timeout 1800
