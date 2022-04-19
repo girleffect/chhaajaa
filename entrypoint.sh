b27bb7e2ccab3d6fc2a080da6c9a5ae0992815b8
Kaitlyn Crawford <kaitlyn@praekelt.com>
2022-04-19 16:57:07 +0200

    Ensure entrypoint script only edits nginx config once

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput 
+python manage.py migrate --noinput
 
-if [ -n "$DOMAIN" ]; then
-    sed -i "s/{{DOMAIN}}/${DOMAIN}/" /etc/nginx/conf.d/django.conf
+if grep "{{DOMAIN}}" /etc/nginx/conf.d/django.conf; then
+    if [ -n "$DOMAIN" ]; then
+        sed -i "s/{{DOMAIN}}/${DOMAIN}/" /etc/nginx/conf.d/django.conf
 
-    target="https://${DOMAIN}\$request_uri"
-    tee -a /etc/nginx/conf.d/django.conf <<EOF
+        target="https://${DOMAIN}\$request_uri"
+        tee -a /etc/nginx/conf.d/django.conf <<EOF
 server {
     listen 8000 default_server;
     server_name _;
     return 301 "${target}";
 }
 EOF
-else
-    sed -i "s/{{DOMAIN}}/_/" /etc/nginx/conf.d/django.conf
+    else
+        sed -i "s/{{DOMAIN}}/_/" /etc/nginx/conf.d/django.conf
+    fi
 fi
 
 nginx -g 'daemon off;' &
