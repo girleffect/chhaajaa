69785265a0aa0b12687dff65f73d72e1982181bb
Shravan Yadav <shravan.simply@gmail.com>
2022-04-28 14:20:14 +0530

    handling emojis in text
    handling emojis in text

--- rapidpro/api/rapidpro/requests/rpp_ftbl_msgs_broadcast_urns.py
+++ rapidpro/api/rapidpro/requests/rpp_ftbl_msgs_broadcast_urns.py
@@ -18,5 +18,6 @@ class BroadcastUrls:
         df = pd.concat(r_n)
         Na = np.nan
         df['contacts']=df['contacts'].apply(lambda x:x[0]['uuid'])
+        df['text_eng']=df['text_eng'].apply(lambda x:str(x).encode('utf-8')[0:256].decode('utf-8'))
         df["broadcast_id"], df["contacturn_id"] = Na, Na
         return df
