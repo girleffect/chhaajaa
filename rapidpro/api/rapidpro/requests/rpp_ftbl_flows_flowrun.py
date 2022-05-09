c4fac2969966c9feba6ae00bb5dbb70aa91b3d09
Shravan Yadav <shravan.simply@gmail.com>
2022-05-09 11:09:10 +0530

    handling null values
    handling null values

--- rapidpro/api/rapidpro/requests/rpp_ftbl_flows_flowrun.py
+++ rapidpro/api/rapidpro/requests/rpp_ftbl_flows_flowrun.py
@@ -19,12 +19,14 @@ class Runs:
         Na = np.nan
         df = df[['id', 'uuid', 'start','responded','created_on', 'modified_on','exited_on', 'exit_type', 'flow_uuid','contact_uuid']]
         df['timeout_on'], df['flowlabel_id'], df['current_node_uuid'], df['message_ids'], df['path'], df['results'], df['session_id'], df['submitted_by_id'], df['is_active'], df['fields'], df['expires_on'], df['contact_id'], df['flow_id'], df['org_id'], df['parent_id'], df['connection_id']= Na, Na,Na, Na,Na, Na,Na, Na,Na, Na,Na, Na,Na, Na,Na, Na
-        df['created_on'] = pd.to_datetime(df['created_on'], format="%Y-%m-%dT%H:%M:%S.%fZ") 
-        df['modified_on']=df['modified_on'].apply(lambda k: ':'.join(k.split(':')[:-1]) + ':' + k.split(':')[-1][:2] + '.00Z' if len(k.split(':')[-1]) < 6 else k)
+        df['created_on'] = pd.to_datetime(df['created_on'], format="%Y-%m-%dT%H:%M:%S.%fZ")
+        if df.shape[0] > 0:
+            df['modified_on']=df['modified_on'].apply(lambda k: ':'.join(k.split(':')[:-1]) + ':' + k.split(':')[-1][:2] + '.00Z' if len(k.split(':')[-1]) < 6 else k)
         df['modified_on'] = pd.to_datetime(df['modified_on'], format="%Y-%m-%dT%H:%M:%S.%fZ") 
         df['expires_on'] = pd.to_datetime(df['expires_on'], format="%Y-%m-%dT%H:%M:%S.%fZ") 
-        df['timeout_on'] = pd.to_datetime(df['timeout_on'], format="%Y-%m-%dT%H:%M:%S.%fZ") 
-        df['exited_on']=df['exited_on'].apply(lambda k: ':'.join(k.split(':')[:-1]) + ':' + k.split(':')[-1][:2] + '.00Z' if len(k.split(':')[-1]) < 6 else k)
+        df['timeout_on'] = pd.to_datetime(df['timeout_on'], format="%Y-%m-%dT%H:%M:%S.%fZ")
+        if df.shape[0] > 0:
+            df['exited_on']=df['exited_on'].apply(lambda k: ':'.join(k.split(':')[:-1]) + ':' + k.split(':')[-1][:2] + '.00Z' if len(k.split(':')[-1]) and k.notnull() < 6 else k)
         df['exited_on'] = pd.to_datetime(df['exited_on'], format="%Y-%m-%dT%H:%M:%S.%fZ") 
         df.rename(columns={'start': 'start_id', 'flow_uuid':'uuid'}, inplace=True)
 
