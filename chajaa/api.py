3419d81456fe55989a83711aa8d68f0adae0bc1f
TheNiqabiCoderMum <ndhedhi@gmail.com>
2022-06-17 16:41:41 +0200

    Create custom endpoint 'services'

--- chajaa/api.py
+++ chajaa/api.py
@@ -1,7 +1,16 @@
-from wagtail.api.v2.views import PagesAPIViewSet
+from wagtail.api.v2.views import BaseAPIViewSet, FieldsFilter
 from wagtail.api.v2.router import WagtailAPIRouter
 
-# "wagtailapi" is the URL namespace
-api_router = WagtailAPIRouter('wagtailapi')
+from service.models import ServicePage
 
-api_router.register_endpoint('p', PagesAPIViewSet)
+
+class ServicesViewSet(BaseAPIViewSet):
+    model = ServicePage
+    filter_backends = [
+      FieldsFilter
+    ]
+
+# "wagtailapi" is the URL namespace
+api_router = WagtailAPIRouter("wagtailapi")
+
+api_router.register_endpoint("services", ServicesViewSet)
