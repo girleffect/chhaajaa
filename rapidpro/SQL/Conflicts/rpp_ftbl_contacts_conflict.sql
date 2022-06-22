c3fa4ab349a38a1da481654d6432bbec4103c371
Shravan Yadav <shravan.simply@gmail.com>
2022-06-22 11:07:35 +0530

    loading the platform information on contacts
    loading the platform information on contacts

--- rapidpro/SQL/Conflicts/rpp_ftbl_contacts_conflict.sql
+++ rapidpro/SQL/Conflicts/rpp_ftbl_contacts_conflict.sql
@@ -1,4 +1,4 @@
-DELETE FROM rappidpro.staging_rpp_ftbl_contact
-USING rappidpro.rpp_ftbl_contact
+DELETE FROM rappidpro.rpp_ftbl_contact
+USING rappidpro.staging_rpp_ftbl_contact
 WHERE rpp_ftbl_contact.uuid = staging_rpp_ftbl_contact.uuid
 and rpp_ftbl_contact.uuid is not null and staging_rpp_ftbl_contact.uuid is not null;
